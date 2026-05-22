eb18965b21ead29b47cd3003668f57cd55bfc1dc
HuuThien-it <nnguyenthien95@gmail.com>
2026-05-22 23:29:16 +0700

    update

---
+++ AppQLTV/AppQLTV/Models/Library/BorrowHistoryModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AppQLTV.Models.Library
+{
+    public class BorrowHistoryModel
+    {
+        public int Id { get; set; }
+        public string ReaderName { get; set; } = "";
+        public DateTime BorrowDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public string Status { get; set; } = "";
+        public string? Note { get; set; }
+    }
+}
---
+++ AppQLTV/AppQLTV/Models/Library/BorrowModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AppQLTV.Models.Library
+{
+    public class BorrowRequest
+    {
+        public int ReaderId { get; set; }
+        public DateTime DueDate { get; set; } = DateTime.Now.AddDays(7);
+        public string? Note { get; set; }
+        public List<BorrowBookItem> Books { get; set; } = new();
+    }
+
+    public class BorrowBookItem
+    {
+        public int BookId { get; set; }
+        public int Quantity { get; set; } = 1;
+    }
+}
