a04719c08a149e71380cf21ce861a839e07a0edb
quannguyen240505-ai <quannguyen.240505@gmail.com>
2026-06-05 15:36:41 +0700

    Fix

--- AppQLTV/APIQLTV/Models/AppDbContext.cs
+++ AppQLTV/APIQLTV/Models/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace APIQLTV.Models
         {
         }
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<LibrarySetting> LibrarySettings { get; set; }
         public DbSet<Reader> Readers { get; set; }
         public DbSet<BorrowTicket> BorrowTickets { get; set; }
         public DbSet<BorrowDetail> BorrowDetails { get; set; }
